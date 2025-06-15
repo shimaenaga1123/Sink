74ed31bcad6e78bf849b6bc25d149f19ace22de9
Kangmin Kim <admin@shimaenaga.dev>
2025-06-15 17:58:18 +0900

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,18 +1,12 @@
 <template>
-  <main class="flex flex-col justify-center">
-    <HomeTwitter />
-    <HomeHero />
-    <HomeFeatures />
-    <HomeLogos />
-    <HomeCta />
+  <main class="flex flex-col justify-center items-center h-screen">
+    <p>리다이렉트 중...</p>
   </main>
 </template>
 
-<script>
-export default {
-  created() {
-    // DOM 생성 전에 바로 리다이렉트
-    window.location.href = 'https://shimaenaga.dev';
-  }
-}
+<script setup>
+import { onMounted } from 'vue';
+onMounted(() => {
+  window.location.href = 'https://shimaenaga.dev';
+});
 </script>
